8a6aa81759876175003df0feadb8f336402be415
Ben Walsh <bwalsh62@gmail.com>
2021-02-15 22:30:31 -0600

    Adding descriptions to feature names when one hot encoding

--- ml_util.py
+++ ml_util.py
@@ -8,6 +8,9 @@ import pandas as pd
 def df_one_hot_encode(df, col):
     # Generate encoding
     oh_encoding = pd.get_dummies(df[col])
+    
+    # Rename columns to be more descriptive in cases where there are many IDs
+    oh_encoding.columns = [col+'_'+str(val) for val in oh_encoding.columns]
 
     # Concatenate new columns to dataframe
     df = pd.concat([df, oh_encoding], axis=1)
